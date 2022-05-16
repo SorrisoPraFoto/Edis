50685363842799ed8a6f41f51206c57660c0f4ce
MARCUS VINICIUS CARUSO LEITE <marcus.caruso@embraer.com.br>
2022-05-16 19:23:31 -0300

    feat: framework com dependencias

---
+++ src/main/java/edis/project/rest/EdisApplication.java
@@ -0,0 +1,13 @@
+package edis.project.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edis/project/rest/EdisApplicationTests.java
@@ -0,0 +1,13 @@
+package edis.project.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
